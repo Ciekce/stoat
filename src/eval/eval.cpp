a38f0a32587da0ccd290c904a273c3185a946df8
Ciekce <44617491+Ciekce@users.noreply.github.com>
2025-01-28 06:36:45 +0000

    handtuned?? mobility
    Bench: 106899111

--- src/eval/eval.cpp
+++ src/eval/eval.cpp
@@ -18,10 +18,20 @@
 
 #include "eval.h"
 
+#include "../attacks/attacks.h"
 #include "material.h"
 
 namespace stoat::eval {
     namespace {
+        constexpr Score kLanceMobility = 7;
+        constexpr Score kKnightMobility = 13;
+        constexpr Score kSilverMobility = 16;
+        constexpr Score kGoldMobility = 16;
+        constexpr Score kBishopMobility = 6;
+        constexpr Score kRookMobility = 7;
+        constexpr Score kPromotedBishopMobility = 6;
+        constexpr Score kPromotedRookMobility = 7;
+
         [[nodiscard]] Score evalMaterial(const Position& pos, Color c) {
             const auto materialCount = [&](PieceType pt) {
                 const auto count = pos.pieceBb(pt, c).popcount();
@@ -62,6 +72,57 @@ namespace stoat::eval {
 
             return score;
         }
+
+        [[nodiscard]] Score evalMobility(const Position& pos, Color c) {
+            const auto ourPieces = pos.colorBb(c);
+            const auto occ = pos.occupancy();
+
+            const auto mobilityCount = [&](Bitboard bb, auto attackGetter) {
+                i32 count{};
+
+                while (!bb.empty()) {
+                    const auto sq = bb.popLsb();
+                    const auto attacks = attackGetter(sq, c) & ~ourPieces;
+                    count += attacks.popcount();
+                }
+
+                return count;
+            };
+
+            const auto golds = pos.pieceBb(PieceTypes::kGold, c) | pos.pieceBb(PieceTypes::kPromotedPawn, c)
+                             | pos.pieceBb(PieceTypes::kPromotedLance, c) | pos.pieceBb(PieceTypes::kPromotedKnight, c)
+                             | pos.pieceBb(PieceTypes::kPromotedSilver, c);
+
+            Score score{};
+
+            score += kLanceMobility * mobilityCount(pos.pieceBb(PieceTypes::kLance, c), [&](Square sq, Color c) {
+                         return attacks::lanceAttacks(sq, c, occ);
+                     });
+
+            score += kKnightMobility * mobilityCount(pos.pieceBb(PieceTypes::kKnight, c), attacks::knightAttacks);
+            score += kSilverMobility * mobilityCount(pos.pieceBb(PieceTypes::kSilver, c), attacks::silverAttacks);
+            score += kGoldMobility * mobilityCount(golds, attacks::goldAttacks);
+
+            score += kBishopMobility * mobilityCount(pos.pieceBb(PieceTypes::kBishop, c), [&](Square sq, Color c) {
+                         return attacks::bishopAttacks(sq, occ);
+                     });
+
+            score += kRookMobility * mobilityCount(pos.pieceBb(PieceTypes::kRook, c), [&](Square sq, Color c) {
+                         return attacks::rookAttacks(sq, occ);
+                     });
+
+            score += kPromotedBishopMobility
+                   * mobilityCount(pos.pieceBb(PieceTypes::kPromotedBishop, c), [&](Square sq, Color c) {
+                         return attacks::promotedBishopAttacks(sq, occ);
+                     });
+
+            score += kPromotedRookMobility
+                   * mobilityCount(pos.pieceBb(PieceTypes::kPromotedRook, c), [&](Square sq, Color c) {
+                         return attacks::promotedRookAttacks(sq, occ);
+                     });
+
+            return score;
+        }
     } // namespace
 
     Score staticEval(const Position& pos) {
@@ -71,6 +132,7 @@ namespace stoat::eval {
         Score score{};
 
         score += evalMaterial(pos, stm) - evalMaterial(pos, nstm);
+        score += evalMobility(pos, stm) - evalMobility(pos, nstm);
 
         return score;
     }
