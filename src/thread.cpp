a431a5f38b67518651606b7a403c807fdf79e186
Ciekce <44617491+Ciekce@users.noreply.github.com>
2025-02-02 02:01:07 +0000

    basic non-capture history, take 2
    Bench: 5475296

--- src/thread.cpp
+++ src/thread.cpp
@@ -34,6 +34,8 @@ namespace stoat {
 
         stats.seldepth.store(0);
         stats.nodes.store(0);
+
+        history.clear();
     }
 
     std::pair<Position, ThreadPosGuard> ThreadData::applyMove(i32 ply, const Position& pos, Move move) {
