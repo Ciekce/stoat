4e9b68dc7b760da3098661d9cf0656ec59c20294
Ciekce <44617491+Ciekce@users.noreply.github.com>
2025-02-14 07:46:21 +0000

    stoatpack
    Bench: 4150106

---
+++ src/datagen/format.h
@@ -0,0 +1,48 @@
+/*
+ * Stoat, a USI shogi engine
+ * Copyright (C) 2025 Ciekce
+ *
+ * Stoat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Stoat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Stoat. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "../types.h"
+
+#include <iostream>
+
+#include "../core.h"
+#include "../move.h"
+#include "../position.h"
+
+namespace stoat::datagen::format {
+    enum class Outcome : u8 {
+        kSenteLoss = 0,
+        kDraw,
+        kSenteWin,
+    };
+
+    class IDataFormat {
+    public:
+        virtual ~IDataFormat() = default;
+
+        virtual void startStandard() = 0;
+        //TODO shogi960, arbitrary position
+
+        virtual void pushUnscored(Move move) = 0;
+        virtual void push(Move move, Score score) = 0;
+
+        virtual void writeAllWithOutcome(std::ostream& stream, Outcome outcome) = 0;
+    };
+} // namespace stoat::datagen::format
