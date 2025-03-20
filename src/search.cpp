ed03bd651fcbfbbd5e7e839d2140a59cd448e835
Ciekce <44617491+Ciekce@users.noreply.github.com>
2025-03-20 07:54:37 +0000

    remove SE
    Bench: 3597372

--- src/search.cpp
+++ src/search.cpp
@@ -575,14 +575,18 @@ namespace stoat {
 
             ++legalMoves;
 
+            i32 extension{};
+
             const auto [newPos, guard] = thread.applyMove(ply, pos, move);
             const auto sennichite = newPos.testSennichite(m_cuteChessWorkaround, thread.keyHistory);
 
             const bool givesCheck = newPos.isInCheck();
 
-            auto newDepth = depth - 1;
+            if (extension == 0 && givesCheck) {
+                extension = 1;
+            }
 
-            i32 extension{};
+            auto newDepth = depth - 1;
 
             Score score;
 
@@ -598,26 +602,7 @@ namespace stoat {
                 goto skipSearch;
             }
 
-            if (!kRootNode && depth >= 7 && ply < thread.rootDepth * 2 && move == ttEntry.move && !curr.excluded
-                && ttEntry.depth >= depth - 3 && ttEntry.flag != tt::Flag::kUpperBound)
-            {
-                const auto sBeta = std::max(-kScoreInf + 1, ttEntry.score - depth * 2);
-                const auto sDepth = (depth - 1) / 2;
-
-                curr.excluded = move;
-                score = search(thread, pos, curr.pv, sDepth, ply, sBeta - 1, sBeta);
-                curr.excluded = kNullMove;
-
-                if (score < sBeta) {
-                    if (!kPvNode && score < sBeta - 16) {
-                        extension = 2;
-                    } else {
-                        extension = 1;
-                    }
-                } else if (ttEntry.score >= beta) {
-                    extension = -1;
-                }
-            } else if (givesCheck) {
+            if (givesCheck) {
                 extension = 1;
             }
 
