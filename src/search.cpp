ca49ea4b015393f07e21d5ae19d0da0ee45ea30e
Ciekce <44617491+Ciekce@users.noreply.github.com>
2025-06-19 12:02:05 +0100

    don't do IIR in expected all-nodes
    Bench: 9625323

--- src/search.cpp
+++ src/search.cpp
@@ -485,7 +485,7 @@ namespace stoat {
                 return ttEntry.score;
             }
 
-            if (depth >= 3 && !ttEntry.move) {
+            if ((kPvNode || expectedCutnode) && depth >= 3 && !ttEntry.move) {
                 --depth;
             }
 
@@ -639,7 +639,6 @@ namespace stoat {
                 r -= pos.isInCheck();
                 r -= move.isDrop() && Square::chebyshev(move.to(), pos.kingSq(pos.stm().flip())) < 3;
                 r += !improving;
-                r += expectedCutnode * 2;
 
                 const auto reduced = std::min(std::max(newDepth - r, 1), newDepth - 1);
                 score = -search(thread, newPos, curr.pv, reduced, ply + 1, -alpha - 1, -alpha, true);
